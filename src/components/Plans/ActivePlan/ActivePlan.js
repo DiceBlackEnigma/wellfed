68d95e836c07cf773ae5e0b40f489432f4a4481c
Oluwadaisi Oluseun Sowemimo <sowemimo@systemspecs.com.ng>
2022-05-16 10:06:17 +0100

    Implement active plan widget

--- src/components/Plans/ActivePlan/ActivePlan.js
+++ src/components/Plans/ActivePlan/ActivePlan.js
@@ -1,16 +1,41 @@
+import {useState} from 'react';
+
 import classes from './ActivePlan.module.css';
 
 export const ActivePlan = props => {
+  const [activeDailyClass, setActiveDailyClass] = useState(true);
+
+  const toggleActiveDailyClassClickHandler = () => {
+    setActiveDailyClass(prevClass => prevClass = !prevClass);
+  };
+
   return (
     <div className={`${classes.ActivePlanWrapper} d-flex`}>
       <div className={`${classes.foodIntakeWidget} background--yellow-tint w-100`}>
         <p className={`text--primary bold-font`}>Today's planned food intake</p>
-        <span>calories</span>
-        <div className={`d-flex justify-content-between`}>
-            <div>
-              <span className={`text--orange bold-font`}>1,450</span>
+        <span onClick={toggleActiveDailyClassClickHandler} className={`text--primary`}>
+          {activeDailyClass ? 'Calories' : 'Cups'}
+        </span>
+        <div>
+          <div style={{overflow: 'hidden', position: 'relative', height: '3.5rem'}}>
+            <div className={`${classes.calorieDailyPlan} ${activeDailyClass ? classes.active : ''}`}>
+              <span style={{'font-size': '2.5rem'}} className={`text--orange bold-font`}>1,450</span>
               <span className={`text--primary`}>kcal</span>
             </div>
+
+            <div className={`${classes.cupDailyPlan} ${!activeDailyClass ? classes.active : ''}`}>
+              <div className={`d-flex`}>
+                <div className={`me-1`}>
+                  <span style={{'font-size': '2.5rem'}} className={`text--orange bold-font`}>3</span>
+                  <span className={`text--primary`}>cups</span>
+                </div>
+                <div>
+                  <span style={{'font-size': '2.5rem'}} className={`text--orange bold-font`}>5</span>
+                  <span className={`text--primary`}>treats</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
